2a7c909d371e4d79d517becc119a445c43b40bfa
Michael Moon <triffid.hunter@gmail.com>
2012-04-17 02:07:52 +1000

    dynamic allocation of UART

--- main.cpp
+++ main.cpp
@@ -19,6 +19,9 @@
 #include "lpc17xx_uart.h"
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_gpio.h"
+#include "lpc17xx_clkpwr.h"
+
+#include <stdio.h>
 
 #define	GPIO_DIR_INPUT 0
 #define	GPIO_DIR_OUTPUT 1
@@ -85,22 +88,28 @@ class UART {
 				PinCfg.Funcnum = 1;
 				PinCfg.OpenDrain = 0;
 				PinCfg.Pinmode = 0;
-				PinCfg.Pinnum = 2;
 				PinCfg.Portnum = 0;
+
+				PinCfg.Pinnum = 2;
 				PINSEL_ConfigPin(&PinCfg);
+
 				PinCfg.Pinnum = 3;
 				PINSEL_ConfigPin(&PinCfg);
+
 				u = (LPC_UART_TypeDef *) LPC_UART0;
 			}
 			else if (port == 1) {
 				PinCfg.Funcnum = 2;
 				PinCfg.OpenDrain = 0;
 				PinCfg.Pinmode = 0;
-				PinCfg.Pinnum = 0;
 				PinCfg.Portnum = 2;
+
+				PinCfg.Pinnum = 0;
 				PINSEL_ConfigPin(&PinCfg);
+
 				PinCfg.Pinnum = 1;
 				PINSEL_ConfigPin(&PinCfg);
+
 				u = (LPC_UART_TypeDef *) LPC_UART1;
 			}
 			else {
@@ -110,9 +119,9 @@ class UART {
 			UART_ConfigStructInit(&UARTConfigStruct);
 
 			UARTConfigStruct.Baud_rate = baud;
-			UARTConfigStruct.Parity = UART_PARITY_NONE;
-			UARTConfigStruct.Databits = UART_DATABIT_8;
-			UARTConfigStruct.Stopbits = UART_STOPBIT_1;
+			//UARTConfigStruct.Parity = UART_PARITY_NONE;
+			//UARTConfigStruct.Databits = UART_DATABIT_8;
+			//UARTConfigStruct.Stopbits = UART_STOPBIT_1;
 
 			UART_Init(u, &UARTConfigStruct);
 
@@ -120,6 +129,40 @@ class UART {
 			UART_FIFOConfig(u, &UARTFIFOConfigStruct);
 
 			UART_TxCmd(u, ENABLE);
+
+			uint8_t ofdr, odlm, odll,olcr,oter;
+
+			olcr = u->LCR;
+			u->LCR = 0x83;
+
+			ofdr = u->FDR;
+			odlm = u->DLM;
+			odll = u->DLL;
+			oter = u->TER;
+
+			//u->FDR = 0x10;
+			//u->DLM = 0;
+			//u->DLL = 25000000 / 16 / baud;
+			u->LCR = 0x03;
+
+			//u->TER = 0x80;
+
+			uint8_t buf[32]; int blen;
+
+			blen = snprintf((char *) buf, sizeof(buf), "PCLK=%lu\n", CLKPWR_GetPCLK(CLKPWR_PCLKSEL_UART0));
+			send(buf, blen);
+			blen = snprintf((char *) buf, sizeof(buf), "LCR=%d\n", olcr);
+			send(buf, blen);
+			blen = snprintf((char *) buf, sizeof(buf), "DIVADDVAL=%d\n", ofdr & 0xF);
+			send(buf, blen);
+			blen = snprintf((char *) buf, sizeof(buf), "MULVAL=%d\n", (ofdr >> 4) & 0xF);
+			send(buf, blen);
+			blen = snprintf((char *) buf, sizeof(buf), "DLM=%d\n", odlm);
+			send(buf, blen);
+			blen = snprintf((char *) buf, sizeof(buf), "DLL=%d\n", odll);
+			send(buf, blen);
+			blen = snprintf((char *) buf, sizeof(buf), "TER=%d\n", oter);
+			send(buf, blen);
 		};
 		~UART() {
 			while (UART_CheckBusy(u) == SET);
@@ -134,8 +177,6 @@ GPIO X (1, 20);
 GPIO Y (1, 25);
 GPIO Z (1, 29);
 GPIO E (0, 10);
-
-UART s (0, 57600);
 
 volatile int g_LoopDummy;
 
@@ -158,16 +199,19 @@ int main()
 
 	setleds(l);
 
-	s.send((uint8_t *) "Start\n", 6);
+	if (1) {
+		UART * s = new UART(0, 9600);
+		s->send((uint8_t *) "Start\n", 6);
 
-    for(;;)
-    {
-        for (int i = 0 ; ((i & (1 << 22)) == 0) && !g_LoopDummy ; i++)
-        {
-        }
-        c[0] = '0' + (l & 7);
-        c[1] = '\n';
-		s.send(c, 2);
-		setleds(l++);
-    }
+		for(;;)
+		{
+    		for (int i = 0 ; ((i & (1 << 22)) == 0) && !g_LoopDummy ; i++)
+    		{
+    		}
+    		c[0] = '0' + (l & 7);
+    		c[1] = '\n';
+			//s->send(c, 2);
+			setleds(l++);
+		}
+	}
 }
