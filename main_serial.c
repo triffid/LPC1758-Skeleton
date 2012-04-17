b0fbc2463d527cbd18f384c9a9399fd8969d4d1d
Michael Moon <triffid.hunter@gmail.com>
2012-04-18 03:42:53 +1000

    add an empty interface and an alternative interface with endpoints because ECM needs it that way

--- main_serial.c
+++ main_serial.c
@@ -83,21 +83,21 @@ void dbgled(int l);
 
 #define BAUD_RATE	115200
 
-#define INT_IN_EP		0x81
-#define BULK_OUT_EP		0x05
-#define BULK_IN_EP		0x82
+#define INT_IN_EP                0x81
+#define BULK_OUT_EP              0x05
+#define BULK_IN_EP               0x82
 
-#define MAX_PACKET_SIZE	64
+#define MAX_PACKET_SIZE          512
 
-#define LE_WORD(x)		((x)&0xFF),((x)>>8)
+#define LE_WORD(x)               ((x)&0xFF),((x)>>8)
 
 // CDC definitions
-#define CS_INTERFACE			0x24
-#define CS_ENDPOINT				0x25
+#define CS_INTERFACE             0x24
+#define CS_ENDPOINT              0x25
 
-#define	SET_LINE_CODING			0x20
-#define	GET_LINE_CODING			0x21
-#define	SET_CONTROL_LINE_STATE	0x22
+#define	SET_LINE_CODING          0x20
+#define	GET_LINE_CODING          0x21
+#define	SET_CONTROL_LINE_STATE   0x22
 
 // data structure for GET_LINE_CODING / SET_LINE_CODING class requests
 typedef struct {
@@ -120,117 +120,6 @@ static fifo_t rxfifo;
 // forward declaration of interrupt handler
 void USBIntHandler(void);
 
-// static const U8 abDescriptors_s[] = {
-// // device descriptor
-// 	0x12,
-// 	DESC_DEVICE,
-// 	LE_WORD(0x0101),			// bcdUSB
-// 	0x02,						// bDeviceClass
-// 	0x00,						// bDeviceSubClass
-// 	0x00,						// bDeviceProtocol
-// 	MAX_PACKET_SIZE0,			// bMaxPacketSize
-// 	LE_WORD(0xFFFF),			// idVendor
-// 	LE_WORD(0x0005),			// idProduct
-// 	LE_WORD(0x0100),			// bcdDevice
-// 	0x01,						// iManufacturer
-// 	0x02,						// iProduct
-// 	0x03,						// iSerialNumber
-// 	0x01,						// bNumConfigurations
-//
-// // configuration descriptor
-// 	0x09,
-// 	DESC_CONFIGURATION,
-// 	LE_WORD(67),				// wTotalLength
-// 	0x02,						// bNumInterfaces
-// 	0x01,						// bConfigurationValue
-// 	0x00,						// iConfiguration
-// 	0xC0,						// bmAttributes
-// 	0x32,						// bMaxPower
-// // control class interface
-// 	0x09,
-// 	DESC_INTERFACE,
-// 	0x00,						// bInterfaceNumber
-// 	0x00,						// bAlternateSetting
-// 	0x01,						// bNumEndPoints
-// 	0x02,						// bInterfaceClass
-// 	0x02,						// bInterfaceSubClass
-// 	0x01,						// bInterfaceProtocol, linux requires value of 1 for the cdc_acm module
-// 	0x00,						// iInterface
-// // header functional descriptor
-// 	0x05,
-// 	CS_INTERFACE,
-// 	0x00,
-// 	LE_WORD(0x0110),
-// // call management functional descriptor
-// 	0x05,
-// 	CS_INTERFACE,
-// 	0x01,
-// 	0x01,						// bmCapabilities = device handles call management
-// 	0x01,						// bDataInterface
-// // ACM functional descriptor
-// 	0x04,
-// 	CS_INTERFACE,
-// 	0x02,
-// 	0x02,						// bmCapabilities
-// // union functional descriptor
-// 	0x05,
-// 	CS_INTERFACE,
-// 	0x06,
-// 	0x00,						// bMasterInterface
-// 	0x01,						// bSlaveInterface0
-// // notification EP
-// 	0x07,
-// 	DESC_ENDPOINT,
-// 	INT_IN_EP,					// bEndpointAddress
-// 	0x03,						// bmAttributes = intr
-// 	LE_WORD(8),					// wMaxPacketSize
-// 	0x0A,						// bInterval
-// // data class interface descriptor
-// 	0x09,
-// 	DESC_INTERFACE,
-// 	0x01,						// bInterfaceNumber
-// 	0x00,						// bAlternateSetting
-// 	0x02,						// bNumEndPoints
-// 	0x0A,						// bInterfaceClass = data
-// 	0x00,						// bInterfaceSubClass
-// 	0x00,						// bInterfaceProtocol
-// 	0x00,						// iInterface
-// // data EP OUT
-// 	0x07,
-// 	DESC_ENDPOINT,
-// 	BULK_OUT_EP,				// bEndpointAddress
-// 	0x02,						// bmAttributes = bulk
-// 	LE_WORD(MAX_PACKET_SIZE),	// wMaxPacketSize
-// 	0x00,						// bInterval
-// // data EP in
-// 	0x07,
-// 	DESC_ENDPOINT,
-// 	BULK_IN_EP,					// bEndpointAddress
-// 	0x02,						// bmAttributes = bulk
-// 	LE_WORD(MAX_PACKET_SIZE),	// wMaxPacketSize
-// 	0x00,						// bInterval
-//
-// 	// string descriptors
-// 	0x04,
-// 	DESC_STRING,
-// 	LE_WORD(0x0409),
-//
-// 	0x0E,
-// 	DESC_STRING,
-// 	'L', 0, 'P', 0, 'C', 0, 'U', 0, 'S', 0, 'B', 0,
-//
-// 	0x14,
-// 	DESC_STRING,
-// 	'U', 0, 'S', 0, 'B', 0, 'S', 0, 'e', 0, 'r', 0, 'i', 0, 'a', 0, 'l', 0,
-//
-// 	0x12,
-// 	DESC_STRING,
-// 	'D', 0, 'E', 0, 'A', 0, 'D', 0, 'C', 0, '0', 0, 'D', 0, 'E', 0,
-//
-// // terminating zero
-// 	0
-// };
-
 static const struct {
 	usbdesc_device				device;
 	usbdesc_configuration	config0;
@@ -239,6 +128,7 @@ static const struct {
 	usbcdc_union					fd_union;
 	usbcdc_ether					fd_ether;
 	usbdesc_endpoint			ep_notify;
+	usbdesc_interface			if_nop;
 	usbdesc_interface			if_data;
 	usbdesc_endpoint			ep_bulkout;
 	usbdesc_endpoint			ep_bulkin;
@@ -275,6 +165,7 @@ static const struct {
 												+ sizeof(usbcdc_union)
 												+ sizeof(usbcdc_ether)
 												+ sizeof(usbdesc_endpoint)
+												+ sizeof(usbdesc_interface)
 												+ sizeof(usbdesc_interface)
 												+ sizeof(usbdesc_endpoint)
 												+ sizeof(usbdesc_endpoint)
@@ -327,11 +218,22 @@ static const struct {
 		.wMaxPacketSize = 8,
 		.bInterval = 10,
 	},
-	.if_data = {
+	.if_nop = {
 		DL_INTERFACE,
 		DT_INTERFACE,
 		.bInterfaceNumber = 1,
 		.bAlternateSetting = 0,
+		.bNumEndPoints = 0,
+		.bInterfaceClass = UC_CDC_DATA,
+		.bInterfaceSubClass = 0,
+		.bInterfaceProtocol = 0,
+		.iInterface = 0,
+	},
+	.if_data = {
+		DL_INTERFACE,
+		DT_INTERFACE,
+		.bInterfaceNumber = 1,
+		.bAlternateSetting = 1,
 		.bNumEndPoints = 2,
 		.bInterfaceClass = UC_CDC_DATA,
 		.bInterfaceSubClass = 0,
@@ -450,23 +352,24 @@ static void BulkIn(U8 bEP, U8 bEPStatus)
  */
 static BOOL HandleClassRequest(TSetupPacket *pSetup, int *piLen, U8 **ppbData)
 {
+	printf("handle %x %x %d %d %d\n", pSetup->bmRequestType, pSetup->bRequest, pSetup->wValue, pSetup->wIndex, pSetup->wLength);
 	switch (pSetup->bRequest) {
 
 	// set line coding
 	case SET_LINE_CODING:
-DBG("SET_LINE_CODING\n");
+		DBG("SET_LINE_CODING\n");
 		memcpy((U8 *)&LineCoding, *ppbData, 7);
 		*piLen = 7;
-DBG("dwDTERate=%u, bCharFormat=%u, bParityType=%u, bDataBits=%u\n",
-	LineCoding.dwDTERate,
-	LineCoding.bCharFormat,
-	LineCoding.bParityType,
-	LineCoding.bDataBits);
+		DBG("dwDTERate=%u, bCharFormat=%u, bParityType=%u, bDataBits=%u\n",
+		LineCoding.dwDTERate,
+		LineCoding.bCharFormat,
+		LineCoding.bParityType,
+		LineCoding.bDataBits);
 		break;
 
 	// get line coding
 	case GET_LINE_CODING:
-DBG("GET_LINE_CODING\n");
+		DBG("GET_LINE_CODING\n");
 		*ppbData = (U8 *)&LineCoding;
 		*piLen = 7;
 		break;
@@ -474,10 +377,11 @@ DBG("GET_LINE_CODING\n");
 	// set control line state
 	case SET_CONTROL_LINE_STATE:
 		// bit0 = DTR, bit = RTS
-DBG("SET_CONTROL_LINE_STATE %X\n", pSetup->wValue);
+		DBG("SET_CONTROL_LINE_STATE %X\n", pSetup->wValue);
 		break;
 
 	default:
+		printf("Unknown CLASS Request: %d\n", pSetup->bRequest);
 		return FALSE;
 	}
 	return TRUE;
